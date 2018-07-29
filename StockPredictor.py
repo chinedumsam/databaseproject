3ef58d9908d4d505c1e6433eab6abc90e6e51655
Unknown <chinedumsam@dal.ca>
2018-07-29 12:30:58 -0300

    import libraries, pandas, matplotlib,quandl,numpy

---
+++ StockPredictor.py
@@ -0,0 +1,19 @@
+# LinearRegression is a machine learning library for linear regression
+from sklearn.linear_model import LinearRegression
+
+# pandas is used for data manipulation
+import pandas as Panda
+
+# matplotlib is used for plotting graphs
+import matplotlib
+
+# sets matplotlib to non interactive output
+matplotlib.use('Agg', warn=False, force=True)
+from matplotlib import pyplot as graph
+
+# quandl is used to fetch data
+import quandl
+
+# save quandl API Key
+quandl.ApiConfig.api_key = "ZnicxoqNgYQA4t56vi_a"
+
