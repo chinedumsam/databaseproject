0c8d1343d55843be300417200714b973231f88c5
erogbogbo seyi <35878285+seyieroz@users.noreply.github.com>
2018-07-29 12:46:59 -0300

    train and test dataset, get dataset from quandl and remove rows with missing values

--- StockPredictor.py
+++ StockPredictor.py
@@ -17,3 +17,46 @@ import quandl
 # save quandl API Key
 quandl.ApiConfig.api_key = "ZnicxoqNgYQA4t56vi_a"
 
+def GetStockPrediction(cpy, cutPoint = .8):
+    print('getting data.....')
+    # get Apple's stock price from 1995 to 2002
+    Data = quandl.get(company, start_date='1995-01-01', end_date='2002-12-31')
+    print('data collected....')
+    # drops other columns except the closing price columns
+    Data = Data[['Close']]
+
+    # removes rows with missing values
+    Data = Data.dropna()
+
+    Data.Close.plot(figsize=(10, 5))
+
+    graph.ylabel("AAPL price variations")
+
+    graph.show()
+
+    # gets the moving averages
+    Data['Ma_3'] = Data['Close'].shift(1).rolling(window=3).mean()
+    Data['Ma_9'] = Data['Close'].shift(1).rolling(window=9).mean()
+
+    Data = Data.dropna()
+
+    X = Data[['Ma_3', 'Ma_9']]
+
+    y = Data['Close']
+
+    cutOffPoint = int(cutPoint * len(Data))
+
+    # Train dataset
+
+    X_train = X[:cutOffPoint]
+
+    y_train = y[:cutOffPoint]
+
+    # Test dataset
+
+    X_test = X[cutOffPoint:]
+
+    y_test = y[cutOffPoint:]
+
+    linear = LinearRegression().fit(X_train, y_train)
+
